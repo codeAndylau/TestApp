e8400a4e654cf90a68bfa499714260d1959f2ffc
lanchao <18782967728@163.com>
2018-09-26 14:24:09 +0800

    git测试  第一次提交

---
+++ TestGit/TestGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Andylau on 2018/9/26.
+//  Copyright © 2018年 Andylau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("iOS 软件开发")
+    }
+
+
+}
+
