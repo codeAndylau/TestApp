981a472e4da1eff572a799e03696947dbf38dfcb
lanchao <18782967728@163.com>
2018-09-26 14:57:54 +0800

    新增pring 输出功能

--- TestGit/TestGit/ViewController.swift
+++ TestGit/TestGit/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("iOS 软件开发")
         print("iOS 软件开发")
+        print("iOS 软件开发")
+        print("iOS 软件开发")
     }
 
 
